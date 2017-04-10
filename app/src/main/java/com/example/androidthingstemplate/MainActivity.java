67c70e9261c581275bb65acc884bd6d96c89af41
Raul Portales <rportales@ov-admins-air-2.oneview-ire.local>
2017-04-10 13:46:09 +0100

    Initial commit

---
+++ app/src/main/java/com/example/androidthingstemplate/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.androidthingstemplate;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Your Android things code goes here
+
+    }
+}
