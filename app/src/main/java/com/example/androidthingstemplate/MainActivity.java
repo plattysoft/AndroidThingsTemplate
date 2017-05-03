e83b3410d981e546c947345898b76a754873d4c0
Raul Portales <rportales@ov-admins-air-2.oneview-ire.local>
2017-05-03 10:04:18 +0100

    proper method onStart on MainActivity, updated gradle, fixed minSdk 24

--- app/src/main/java/com/example/androidthingstemplate/MainActivity.java
+++ app/src/main/java/com/example/androidthingstemplate/MainActivity.java
@@ -1,14 +1,12 @@
 package com.example.androidthingstemplate;
 
 import android.app.Activity;
-import android.os.Bundle;
 
 public class MainActivity extends Activity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
+    protected void onStart() {
+        super.onStart();
         // Your Android things code goes here
 
     }
